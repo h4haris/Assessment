20151cb8a4edfe0047f700ca180171ca47f2b635
h4haris <33560381+h4haris@users.noreply.github.com>
2017-11-11 05:07:52 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+            int sum = 2;
+            while (j < 4000000)
+            {
+                int temp = i;
+                i = j;
+                j = temp + j;
+                if (j % 2 == 0)
+                    sum += j;
+            }
+            Console.WriteLine("Sum is " + sum);
+        }
+    }
+}
